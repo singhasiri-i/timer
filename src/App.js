caab18e1c434d551712d33e722c9ec8462438139
Issariyaporn Singhasiri <singhasiri.i.b@gmail.com>
2024-08-05 20:25:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// Exercise >> https://codesandbox.io/p/sandbox/live-test-basic-javascript-2-8jfx1w?file=%2Fsrc%2Fstyles.css%3A1%2C1-35%2C1
+// Expected >> https://lxcx3i.csb.app/
+// Prev State >> https://www.youtube.com/watch?v=yvTGXH7uybA
+// setInterval >> https://www.youtube.com/watch?v=AqSm-353U8I
+// clearInterval >> https://www.youtube.com/watch?v=n0SpVgt3Lho
 
-function App() {
+
+import { useEffect, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [startCount, setStartCount] = useState(false)
+
+  const countStart = () => {
+    setCount((prev) => prev + 1 )
+  }
+  
+  const handleClickStart = () => {
+    setStartCount(true)
+  }
+  
+  const handleClickStop = () => {
+    setStartCount(false)
+}
+
+useEffect(() => {
+  let intervalId = 0
+  if (startCount) {
+    intervalId = setInterval(() => {
+      countStart()
+    }, 1000)
+  }
+  return () => {
+    clearInterval(intervalId)
+    setStartCount(!startCount)
+    }
+  }, [startCount])
+
+  // console.log("Count after useEffect()",count);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test Basic React 2</h1>
+      <div className="count">{count < 10 ? "0" + count : count}</div>
+      <div>
+        <button onClick={handleClickStart}>start</button>
+        <button onClick={handleClickStop}>stop</button>
+        <button onClick={() => setCount(0)}>reset</button>
+      </div>
     </div>
   );
 }
-
-export default App;
